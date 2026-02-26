1ce52916de0f2f5bdd07ea1280348bd49c589499
Aniket Kumar <aniketkumar10b112028@gmail.com>
2026-02-26 17:23:34 +0530

    Update DT.py

--- DT.py
+++ DT.py
@@ -10,7 +10,7 @@ y = [0,1,1,1,0,0,1]
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+X_test = 20
 # Model
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
